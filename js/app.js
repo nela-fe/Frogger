2f50c46e46d5712ec02facad14b867362646cdec
Angela Laub <angela.laub@gmx.de>
2019-05-21 21:34:55 +0200

    move player

--- js/app.js
+++ js/app.js
@@ -1,35 +1,67 @@
 // Enemies our player must avoid
-var Enemy = function() {
     // Variables applied to each of our instances go here,
     // we've provided one for you to get started
 
+class Enemy {
+    constructor() {
     // The image/sprite for our enemies, this uses
     // a helper we've provided to easily load images
-    this.sprite = 'images/enemy-bug.png';
-};
-
-// Update the enemy's position, required method for game
-// Parameter: dt, a time delta between ticks
-Enemy.prototype.update = function(dt) {
+        this.sprite = 'images/enemy-bug.png';
+    }
+    // Update the enemy's position, required method for game
+    // Parameter: dt, a time delta between ticks
     // You should multiply any movement by the dt parameter
     // which will ensure the game runs at the same speed for
     // all computers.
-};
 
-// Draw the enemy on the screen, required method for game
-Enemy.prototype.render = function() {
-    ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
-};
+    update(dt){
+
+    }
+    // Draw the enemy on the screen, required method for game
+    render(){
+        ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+
+    }
+}
+
 
 // Now write your own player class
 // This class requires an update(), render() and
 // a handleInput() method.
+
+class Player {
+    constructor() {
+        this.sprite = 'images/char-cat-girl.png';
+        this.x = 207;
+        this.y = 300;
+    }
+    update(dt){
+    }
+    render(){
+        ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+    }
+
+    handleInput(pressed){
+        if(pressed == "left" &&  this.x >= 106) {
+            this.x -= 101;
+        } else if(pressed == "right" && this.x <= 308){
+            this.x += 101;
+        } else if(pressed == "up" && this.y >= 51){
+            this.y -= 83;
+        } else if(pressed == "down" && this.y <= 300){
+            this.y += 83;
+        }
+    // console.log(this.x, this.y);
+    }
+}
 
 
 // Now instantiate your objects.
 // Place all enemy objects in an array called allEnemies
 // Place the player object in a variable called player
 
+const allEnemies = [];
+const player = new Player();
 
 
 // This listens for key presses and sends the keys to your
