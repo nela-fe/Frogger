857b2338d770023e235306abb18be7ab1dc5d62f
Angela Laub <angela.laub@gmx.de>
2019-05-26 18:13:34 +0200

    tidy up comments

--- js/app.js
+++ js/app.js
@@ -1,28 +1,14 @@
-//////////////
-//  ENEMY   //
-//////////////
+/*-------------
+     ENEMY
+---------------*/
 
 class Enemy {
-    // constructor(x, y, speed) {
-    //     this.sprite = 'images/enemy-bug_dark.png';
-    //     this.x = x;
-    //     this.y = y;
-    //     this.speed = speed;
-    // }
-
-    constructor(y) {
+    constructor(lane) {
         this.sprite = 'images/enemy-bug_dark.png';
-        this.x = getRandomInt(-300, -100);
-        this.y = y;
-        this.speed = getRandomInt(60, 80);
-
-        // new Enemy(getRandomInt(-300, -100), 60, getRandomInt(60, 80)), // x, y, speed
-        // new Enemy(getRandomInt(-300, -100), 143, getRandomInt(70, 90)),
-        // new Enemy(getRandomInt(-300, -100), 226, getRandomInt(45, 60))
-
-    }
-
-
+        this.x = getRandomInt(-500, -100);
+        this.y = lane;
+        this.speed = getRandomInt(40, 120);
+     }
 
     // Update the enemy's position, required method for game
     // Parameter: dt, a time delta between ticks
@@ -31,20 +17,16 @@ class Enemy {
     // all computers.
 
     update(dt){
-        // when bug has vanished to the right (>=500),
-        // it reappears from the left (between -300 and -100)
-        // with a new random speed
-
-        if (this.x >= 505){
-            // let yPos = this.y;
-            //  new Enemy(yPos); // doesn't work
-
-            //duplicate code...
-            this.x = getRandomInt(-300, -100);  // oder: erzeuge einen neuen?
-            this.speed = getRandomInt(45, 120); // effektiv ist es ein neuer :)
-            for (i = 0; i < allEnemies.length; i++){
-                console.log("Speed: "+allEnemies[i].speed);
-            }
+        // when bug has vanished to the right,
+        // it reappears from the left with a random distance and speed
+        if (this.x >= 505) {
+            // how can I get rid of this duplicate code...
+            // delete Enemy and make new one with constructor?
+            this.x = getRandomInt(-500, -100);
+            this.speed = getRandomInt(40, 120);
+            // for (i = 0; i < allEnemies.length; i++){
+            //     console.log("Speed: "+allEnemies[i].speed);
+            // }
         } else {
         this.x += dt*this.speed;
         }
@@ -56,22 +38,19 @@ class Enemy {
 }
 
 
-// more than one enemy per lane?
+// TODO: more than one enemy per lane, prevent collisions between them
 const allEnemies = [
+    // new Enemy(60),
+    // new Enemy(143),
+    // new Enemy(226),
     new Enemy(60),
     new Enemy(143),
     new Enemy(226)
-    // new Enemy(getRandomInt(-300, -100), 60, getRandomInt(60, 80)), // x, y, speed
-    // new Enemy(getRandomInt(-300, -100), 143, getRandomInt(70, 90)),
-    // new Enemy(getRandomInt(-300, -100), 226, getRandomInt(45, 60))
-    // new Enemy(-400, 60, 65), // x, y, speed
-    // new Enemy(-200, 143, 45),
-    // new Enemy(-100, 226, 80),
 ];
 
-for (i = 0; i < allEnemies.length; i++){
-    console.log("Speed: "+allEnemies[i].speed);
-}
+// for (i = 0; i < allEnemies.length; i++){
+//     console.log("Speed: "+allEnemies[i].speed);
+// }
 
 // from https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Math/random
 function getRandomInt(min, max) {
@@ -82,12 +61,11 @@ function getRandomInt(min, max) {
 
 
 
+/*-------------
+     PLAYER
+---------------*/
 
 
-
-//////////////
-//  PLAYER  //
-//////////////
 
 class Player {
     constructor(x, y) {
@@ -98,7 +76,6 @@ class Player {
         this.maxStars = 5; // 5
         this.maxHearts = 5; // 5
         this.hearts = this.maxHearts;
-
         this.arrivedAtWater = false;
     }
 
@@ -113,38 +90,29 @@ class Player {
         this.y = 383;
     }
 
-    /*
-   //  OR a reset game function
-    resetPlayerStats(){
-
-
-    }
-    */
-
-
     handleInput(pressed){
         // console.log(player.y);
-        if(player.y >= 51){ // disable moving while at water to prevent "walking on water" :D
-            if(pressed == "left" &&  this.x >= 102) {
+        if (player.y >= 51) { // disable moving while at water to prevent "walking on water" :D
+            if (pressed == "left" &&  this.x >= 102) {
                 this.x -= 101;
-            } else if(pressed == "right" && this.x <= 304){
+            } else if (pressed == "right" && this.x <= 304) {
                 this.x += 101;
-            } else if(pressed == "up" && this.y >= 51){
+            } else if (pressed == "up" && this.y >= 51) {
                 this.y -= 83;
-            } else if(pressed == "down" && this.y <= 300){
+            } else if (pressed == "down" && this.y <= 300) {
                 this.y += 83;
             }
         }
 
 // another possibility would be to remove the eventListener for that time
 
-        if (this.y <=50){ // player has arrived at water
+        if (this.y <=50) { // player has arrived at water
             // console.log(this.arrivedAtWater);
-            if (this.arrivedAtWater == false){   // prevent multiple key presses during timeOut resulting in multiple stars
+            if (this.arrivedAtWater == false) {   // prevent multiple key presses during timeOut resulting in multiple stars
                 this.arrivedAtWater = true;
                 // console.log(this.arrivedAtWater);
                 this.addStars()
-                var myTimeout = setTimeout(function(){
+                var myTimeout = setTimeout(function() {
                     player.resetPlayer();
                     player.arrivedAtWater = false;
                 }, 400);
@@ -153,43 +121,37 @@ class Player {
     }
 
     addStars(){
-        if(this.stars < this.maxStars) {
+        if (this.stars < this.maxStars) {
         // console.log("player has "+this.stars +" stars.");
         this.stars++;
         // console.log("player has "+this.stars +" stars.");
         }
-        if(this.stars == this.maxStars){
+        if (this.stars == this.maxStars) {
             // console.log('you win!!');
             gameOver('won');
         }
     }
 
     reduceHearts(){
-        if(this.hearts > 0) {
+        if (this.hearts > 0) {
             // console.log('you lost one life');
             this.hearts--;
         }
-        if(this.hearts == 0) {
+        if (this.hearts == 0) {
             // console.log('you lost the game');
             gameOver('lost');
-
         }
     }
-
-
-    // TODO: when game is over (lost or won) show modal that asks if player
-    // wants to play again. --> reset game
-
 }
-
-
-
-
-
 
 
 const player = new Player(203, 383);
 
+
+
+/*----------------
+     Control
+-----------------*/
 
 // This listens for key presses and sends the keys to your
 // Player.handleInput() method. You don't need to modify this.
@@ -205,23 +167,27 @@ document.addEventListener('keyup', function(e) {
 });
 
 
-// TODO: put this in either player or enemy?
-
+/*------------------------
+     Collision Detection
+-------------------------*/
 // loop over the enemies and check for each one whether enemy and player occupy the same space
 // condition is met when enemy x is larger than player x minus width of enemy (101)
 // and enemy x is smaller than player x plus player width (image 66, margin 17,5)
-function checkCollisions(){
+
+// TODO: move this into enemy? or into enemy prototype..?
+
+function checkCollisions() {
     allEnemies.forEach(function(enemy) {
-        if(player.y == enemy.y - 9
+        if (player.y == enemy.y - 9
             && enemy.x > (player.x - 83)
-            && enemy.x < (player.x + 66)){ // works better than 83
+            && enemy.x < (player.x + 66)) { // works better than 83
 
                 // console.log('gotcha!');
                 player.reduceHearts();
                 player.resetPlayer();
+                // Vehicle-player collision resets the game?
 
-
-                // if I use timeOut, I need another variable (like arrivedAtWater)
+                // if I use timeOut, I need another variable (like with arrivedAtWater)
                 // to check if this is the first time the collision was noticed,
                 // to prevent the code from being executed multiple times
 
@@ -229,9 +195,8 @@ function checkCollisions(){
                 var myTimeout = setTimeout(function(){
                     player.resetPlayer();
 
-                }, 1000);
+                }, 400);
                 */
-
             }
     });
 }
@@ -251,32 +216,36 @@ Player  Enemy
 
 
 
+
+
+/*---------------------
+     Start / Stop
+----------------------*/
+
+
 //TODO: put this into Enemy Class (?)
-
-
-
 
 // Start and stop enemies
 
 let stop = false;
 let enemySpeeds = [] // to store the enemies' speed values while they are stopped
 
-function stopEnemies(){
-        allEnemies.forEach(function(enemy){
+function stopEnemies() {
+        allEnemies.forEach(function(enemy) {
             enemySpeeds.push(enemy.speed);
             enemy.speed = 0;
         });
     }
 
-function startEnemies(){
-        for (i = 0; i < allEnemies.length; i++){
+function startEnemies() {
+        for (i = 0; i < allEnemies.length; i++) {
             allEnemies[i].speed = enemySpeeds[i];
         }
         enemySpeeds = [];
     }
 
 // use "s" key to toggle between stopping / starting the bugs (for testing)
-document.addEventListener('keyup', function(e){
+document.addEventListener('keyup', function(e) {
     if (e.keyCode == 83) { // 's'
         if (stop == false) {
             stopEnemies();
@@ -289,20 +258,18 @@ document.addEventListener('keyup', function(e){
 });
 
 
+/*------------------
+     Game Over
+-------------------*/
 
 
-
-function gameOver(condition){
+function gameOver(condition) {
     // console.log('GAME OVER! You ' + condition + ' the game');
     stopEnemies();
-
     showModal(condition);
 
-
     // TODO: remove event Listener
-    // TODO: show modal that game is lost/won (pass condition), play again?
     // TODO: Reset game
-
 
 
     /*
@@ -319,7 +286,6 @@ function gameOver(condition){
         player.handleInput(allowedKeys[e.keyCode]);
     });
 */
-
 }
 
 
@@ -328,7 +294,7 @@ function gameOver(condition){
      MODAL
 ---------------*/
 
-function showModal(winOrLose){
+function showModal(winOrLose) {
 
     let wonOrLost = winOrLose;
     // console.log('condition: ' + wonOrLost);
@@ -340,14 +306,10 @@ function showModal(winOrLose){
     // console.log(messageLine);
     messageLine.insertAdjacentHTML('afterbegin', 'You ' + wonOrLost + ' the game');
 
-
+    // TODO: make buttons clickable
     // let buttonPlayAgain = document.querySelector('#playAgain');
     // console.log(buttonPlayAgain);
 
     // let buttonNoThanks = document.querySelector('#noThanks');
     // console.log(buttonNoThanks);
-
 }
-
-
-
